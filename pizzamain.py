9a40f2b469dff5b5278d8dcfcf7c837ca42a0136
sunkyu0711 <sunkyu0711@gmail.com>
2025-01-15 11:21:00 +0900

    pizzaorder.py의 수정에 따른 수정

--- pizzamain.py
+++ pizzamain.py
@@ -8,22 +8,21 @@
 
 from pizzaorder import *
 
-pmenu, porder=Pizza()
-dmenu, dorder=Drink()
+pizza_menu={'페퍼로니 피자':3000,
+            '치즈 피자':3200,
+            '콤비네이션 피자':3500,
+            '불고기 피자':3600,
+            '해산물 피자':3800}
+drink_menu={'콜라':1500,
+            '사이다':1500,
+            '생수':1000}
 
-pizza_price=0
-print('='*25)
-print('주문 내역:')
-for pizza in porder:
-    print(f'- {pizza} ({pmenu[pizza]}원) x {porder[pizza]} = {int(pmenu[pizza])*int(porder[pizza])}원')
-    pizza_price=pizza_price+(int(pmenu[pizza])*int(porder[pizza]))
+pizza_order = Order(pizza_menu,'피자')
+print(pizza_order)
+drink_order = Order(drink_menu,'음료수')
+print(drink_order)
 
-print(f'총 금액: {pizza_price}원')
+pizza_price=Money(pizza_order,pizza_menu,'피자')
+drink_price=Money(drink_order,drink_menu,'음료수')
 
-
-drink_price=0
-print('주문 내역:')
-for drink in dorder:
-    print(f'- {drink} ({dmenu[drink]}원) x {dorder[drink]} = {int(dmenu[drink])*int(dorder[drink])}원')
-    drink_price=drink_price+(int(dmenu[drink])*int(dorder[drink]))
-print(f'총 금액: {drink_price}원')
+print(f'전체 가격: {pizza_price+drink_price:,}원')
