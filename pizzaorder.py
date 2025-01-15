b87c285583105d2b88a9fe57bcf5505bebabc6ab
sunkyu0711 <sunkyu0711@gmail.com>
2025-01-15 11:15:31 +0900

    함수 추가 및 수정

--- pizzaorder.py
+++ pizzaorder.py
@@ -1,50 +1,70 @@
 # [9일 차 문제2] 피자와 음료수를 주문한 뒤 영수증을 출력하는 프로그램을 모듈을 이용하여 만들어 보세요.
+# 아래 코드는 모범 답안과 엮은 결과: 중복을 줄이려는 노력을 해야!
 
-def Pizza():
-    pizza_menu={'페퍼로니 피자':'3000',
-                '치즈 피자':'3200',
-                '콤비네이션 피자':'3500',
-                '불고기 피자':'3600',
-                '해산물 피자':'3800'}
-    print('다음은 피자 메뉴입니다.')
-    for pizza in pizza_menu:
-        print(f'{pizza} ({pizza_menu[pizza]} 원)')
+def Order(menu,name):
+    print(f'\n다음은 {name} 메뉴입니다.')
+    for selection in menu: # or for name, price in menu.items():
+        print(f'{selection} ({menu[selection]:,}원)')
     
-    pizza_order={}
+    order={}
 
     while True:
-        pizza_menu_selection=input('주문할 메뉴를 입력하세요(종료는 exit): ')
-        if pizza_menu_selection=='exit':
+        menu_selection=input(f'주문할 {name}를 입력하세요(종료는 exit): ')
+        if menu_selection=='exit':
             break
         else:
-            pizza_num_selection=int(input('수량을 입력하세요: '))
-            pizza_order[pizza_menu_selection]=pizza_num_selection
+            num_selection=int(input('수량을 입력하세요: '))
+            order[menu_selection]=num_selection
 
-    return pizza_menu, pizza_order
+    return order
 
-def Drink():
-    drink_menu={'콜라':'1500',
-                '사이다':'1500',
-                '생수':'1000'}
-    print('다음은 음료수 메뉴입니다.')
-    for drink in drink_menu:
-        print(f'{drink} ({drink_menu[drink]} 원)')
-    
-    drink_order={}
+def Money(order,menu,name):
+    price=0
+    print('='*25)
+    print(f'{name} 주문 내역:')
+    for i in order.keys():
+        if i in menu.keys():
+            price=price+(order[i]*menu[i])
+        print(f'- {i} ({menu[i]:,}원) x {order[i]} = {menu[i]*order[i]:,}원')
+    print(f'주문한 {name}의 총 금액: {price:,}원')
+    return price
 
-    while True:
-        drink_menu_selection=input('주문할 메뉴를 입력하세요(종료는 exit): ')
-        if drink_menu_selection=='exit':
-            break
-        else:
-            drink_num_selection=int(input('수량을 입력하세요: '))
-            drink_order[drink_menu_selection]=drink_num_selection
-
-    return drink_menu, drink_order
 
 if __name__=='__main__':
-    Pizza()
-    Drink()
+    pizza_menu={'페퍼로니 피자':3000,
+                '치즈 피자':3200,
+                '콤비네이션 피자':3500,
+                '불고기 피자':3600,
+                '해산물 피자':3800}
+    drink_menu={'콜라':1500,
+                '사이다':1500,
+                '생수':1000}
+
+    # 주문
+    pizza_order = Order(pizza_menu,'피자')
+    print(pizza_order)
+    drink_order = Order(drink_menu,'음료수')
+    print(drink_order)
+
+    # 계산
+    pizza_price=Money(pizza_order,pizza_menu,'피자')
+    drink_price=Money(drink_order,drink_menu,'음료수')
+
+    print(f'전체 가격: {pizza_price+drink_price:,}원')
+
+    
+
+
+
+
+    # drink_price=0
+    # print('주문 내역:')
+    # for drink in dorder:
+    #     print(f'- {drink} ({dmenu[drink]}원) x {dorder[drink]} = {int(dmenu[drink])*int(dorder[drink])}원')
+    #     drink_price=drink_price+(int(dmenu[drink])*int(dorder[drink]))
+    # print(f'총 금액: {drink_price}원')
+
+    # Drink()
 
 # if __name__=='__main__':
 #     pmenu, porder=Pizza()
