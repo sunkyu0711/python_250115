30cde77b0c38c24ae6962208e5adfbdc3640345e
sunkyu0711 <sunkyu0711@gmail.com>
2025-01-15 11:29:08 +0900

    단축키 설명 추가 및 if 문 삭제

--- pizzaorder.py
+++ pizzaorder.py
@@ -1,4 +1,5 @@
 # [9일 차 문제2] 피자와 음료수를 주문한 뒤 영수증을 출력하는 프로그램을 모듈을 이용하여 만들어 보세요.
+# 여러 줄 탭 키 지우기 - 드래그 후 shift+tab
 # 아래 코드는 모범 답안과 엮은 결과: 중복을 줄이려는 노력을 해야!
 
 def Order(menu,name):
@@ -28,24 +29,3 @@ def Money(order,menu,name):
         print(f'- {i} ({menu[i]:,}원) x {order[i]} = {menu[i]*order[i]:,}원')
     print(f'주문한 {name}의 총 금액: {price:,}원')
     return price
-
-
-if __name__=='__main__':
-    pizza_menu={'페퍼로니 피자':3000,
-                '치즈 피자':3200,
-                '콤비네이션 피자':3500,
-                '불고기 피자':3600,
-                '해산물 피자':3800}
-    drink_menu={'콜라':1500,
-                '사이다':1500,
-                '생수':1000}
-
-    pizza_order = Order(pizza_menu,'피자')
-    print(pizza_order)
-    drink_order = Order(drink_menu,'음료수')
-    print(drink_order)
-
-    pizza_price=Money(pizza_order,pizza_menu,'피자')
-    drink_price=Money(drink_order,drink_menu,'음료수')
-
-    print(f'전체 가격: {pizza_price+drink_price:,}원')
